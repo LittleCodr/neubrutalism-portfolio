e02cd2c74e0434dc3d9b51af6686e44aa20e3607
LittleCodr <mayankheroagrawal25@gmail.com>
2025-09-27 00:28:07 +0530

    jy

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -83,7 +83,7 @@ const Navbar = () => {
           ))}
         </div>
 
-        {/* Mobsighfjle Navisdsdsadagation Toggle */}
+        {/* Mobsighfjle Navisgukdsdsadasasadasgation Toggle */}
         <button 
           className="md:hidden neubrutalism-button glass"
           onClick={() => setIsOpen(!isOpen)}

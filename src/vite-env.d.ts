5391976f6d9932728543e87ee83bdbce31e81395
LittleCodr <mayankheroagrawal25@gmail.com>
2025-09-27 23:59:13 +0530

    uighui

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,19 @@
 /// <reference types="vite/client" />
+
+// Web Speech API Type Definitions
+type SpeechRecognition = any;
+type SpeechGrammarList = any;
+type SpeechRecognitionEvent = any;
+
+declare global {
+  interface Window {
+    SpeechRecognition: typeof SpeechRecognition;
+    webkitSpeechRecognition: typeof SpeechRecognition;
+    SpeechGrammarList: typeof SpeechGrammarList;
+    webkitSpeechGrammarList: typeof SpeechGrammarList;
+    SpeechRecognitionEvent: typeof SpeechRecognitionEvent;
+    webkitSpeechRecognitionEvent: typeof SpeechRecognitionEvent;
+  }
+}
+
+export {};
